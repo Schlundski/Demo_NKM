d93f17d6472eed29cd59b09bc898d7f75096935b
Schlundski <nico_schwarz@gmx.de>
2025-11-03 08:55:55 +0100

    Wir kümmern um das Implementieren der Berechnungen

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ check_login()
 #Startseite generieren, damit app nicht links steht in der Navigation
 def startseite():
     st.image("image/Noell.jpg")
-    st.write("# 🏭Willkommen! \nZuerst müssen Sie die Daten der Bestandsanlage eingeben und speichern. \n\nDann können Sie Änderungen eingeben und in Echt-Zeit eine Auswertung der faktorenspezifischen Daten erhalten. \n\nWenn Sie bereit sind, drücken sie die Taste \"Eingabe der Daten der Anlage\".")
+    st.markdown("""
+             # 🏭Willkommen!
+             Zuerst werden Sie durch die Dateneingabe Ihrer Bestandsanlage geführt.\n
+             Dann können Sie ihre Eingaben nochmals überprüfen und speichern.\n
+             Abschließend kommt eine Auswertung der Eingaben. Dort können Sie auch Erneuerungen vornehmen und der Unterschied wird anschaulich Visualisiert.\n
+             Wenn Sie bereit sind, drücken sie die Taste \"Eingabe der Daten der Anlage\".
+             """)
+             
 
     if st.button(label="Eingabe der Daten der Anlage"):
         st.switch_page("pages/Faktoren.py")
